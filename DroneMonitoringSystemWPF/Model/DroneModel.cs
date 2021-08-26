a19296eb0cfe07685171b92bcef5c015a8bc81e4
이상협 <hacker6462@gmail.com>
2021-08-27 00:31:01 +0900

    초기 커밋.

---
+++ DroneMonitoringSystemWPF/Model/DroneModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneMonitoringSystemWPF.Model
+{
+    class DroneModel
+    {
+        public DroneModel(int sysid, double latitude, double longitude)
+        {
+            SysID = sysid;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public int SysID { get; set; }
+    }
+}
---
+++ DroneMonitoringSystemWPF/ViewModel/DroneViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using DroneMonitoringSystemWPF.Commands;
+using DroneMonitoringSystemWPF.Model;
+
+namespace DroneMonitoringSystemWPF.ViewModel
+{
+    class DroneViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<DroneModel> droneStatus;
+        public ObservableCollection<DroneModel> DroneStatus
+        {
+            get
+            {
+                return droneStatus;
+            }
+            set
+            {
+                droneStatus = value;
+                OnPropertyChanged("droneStatus");
+            }
+        }
+        public DelegateCommand DroneMarkerClickCommand { get; set; }
+        public DelegateCommand ConstructDroneImageCommand { get; set; }
+        public BitmapImage DroneImage { get; set; }
+
+        public DroneViewModel()
+        {
+            DroneStatus = new ObservableCollection<DroneModel>();
+            DroneMarkerClickCommand = new DelegateCommand(DroneMarkerClick);
+            ConstructDroneImageCommand = new DelegateCommand(ConStructDroneImage);
+        }
+
+        
+
+
+
+        #region Commands
+        private void ConStructDroneImage(object parameter)
+        {
+            //int d = int.Parse(parameter.ToString());
+            var droneModel = new DroneModel(0,120,120);
+            //DroneImage = new Bitmap("E:\\Drone Projects\\DroneMonitoringSystemWPF\\DroneMonitoringSystemWPF\\Resources\\Hoya.png");
+            DroneImage = new BitmapImage(new Uri("E:\\Drone Projects\\DroneMonitoringSystemWPF\\DroneMonitoringSystemWPF\\Resources\\Hoya.png"));
+            DroneStatus.Add(droneModel);
+        }
+
+
+        private void DroneMarkerClick(object parameter)
+        {
+            //var selectedDroneStatus = parameter as ObservableCollection<DroneModel>;
+            var drone = (parameter as ObservableCollection<DroneModel>).Where(z => z.SysID == 0).FirstOrDefault();
+            if(drone != null)
+            {
+                Console.WriteLine("Exist!!");
+            }
+            else
+            {
+                Console.WriteLine("NOP!!");
+            }
+        }
+        #endregion
+
+
+        #region PropertyChange
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+    }
+}
